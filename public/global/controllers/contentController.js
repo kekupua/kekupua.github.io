b172785ddaefc79900cf44079cdc8705870c64b0
Kekupua <seteramae@gmail.com>
2016-08-15 23:24:00 -1000

    Added option selector for expansions

--- public/global/controllers/contentController.js
+++ public/global/controllers/contentController.js
@@ -1,11 +1,29 @@
 kekupua.controller('ContentController', ['$scope', '$mdDialog', 'globalService' , function($scope, $mdDialog, globalService){
   $scope.allCards = [];
+  $scope.currentExpansion = 'Karazhan';
+  $scope.cardSet = 'Karazhan';
   $scope.status = null;
 
-  $scope.loadCards = function(){
+  $scope.expansions = {
+    "Basic": 0,
+    "Blackrock Mountain": 1,
+    "Classic": 2,
+    "Goblins vs Gnomes": 5,
+    "Karazhan": 7,
+    "Naxxramas": 9,
+    "The Grand Tournament": 14,
+    "The League of Explorers": 15,
+    "Whispers of the Old Gods": 16,
+  }
+
+  $scope.loadCards = function(cardSet){
+    if(cardSet == null) return;
+    if(cardSet == $scope.currentExpansion && $scope.status == 1) return;
+    $scope.currentExpansion = cardSet;
+    $scope.status = null;
     globalService.getCards(null, function(response){
       $scope.allCards = response;
-      $scope.chunkedCards = $scope.chunk($scope.allCards['Whispers of the Old Gods'], 8);
+      $scope.chunkedCards = $scope.chunk($scope.allCards[cardSet], 8);
       $scope.status = 1;
     });
   }
@@ -32,6 +50,6 @@ kekupua.controller('ContentController', ['$scope', '$mdDialog', 'globalService' 
         });
   }
 
-  $scope.loadCards();
+  $scope.loadCards('Karazhan');
 
 }]);
