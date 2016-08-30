c5a6b1c7d8cf718fb195d65870ff9dba26eda638
Kekupua <seteramae@gmail.com>
2016-08-29 21:10:17 -1000

    For Mel

--- public/global/states.js
+++ public/global/states.js
@@ -1,6 +1,6 @@
 kekupua.config(function($stateProvider, $urlRouterProvider) {
   // For any unmatched url, redirect to /
-  $urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise("/test");
   // Now set up the states
   $stateProvider
     .state('home', {
@@ -8,9 +8,9 @@ kekupua.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "public/global/views/home.html",
       controller: "ContentController"
     })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "partials/state2.html"
+    .state('test', {
+      url: "/test",
+      templateUrl: "public/global/views/test.html"
     })
     .state('state2.list', {
       url: "/list",
---
+++ public/personal/controllers/personalController.js
@@ -0,0 +1,10 @@
+kekupua.controller('PersonalController', ['$scope', function($scope){
+  
+  $scope.carouselImages = [
+    {src: 'public/resources/images/instagram.jpg'},
+    {src: 'public/resources/images/datahouseVolunteer.JPG'},
+    {src: 'public/resources/images/withKoffing.jpg'},
+    {src: 'public/resources/images/withTerry.jpg'}
+  ];
+
+}]);
