0831f186946b710d0817f68d2076a35a686a4be0
Kekupua <seteramae@gmail.com>
2018-01-14 14:14:16 -1000

    Demos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,61 @@
 /**
  * Created by Sean on 9/24/2017.
  */
-// var Vue = require('vue');
-
-Vue.component('nav-banner', {
-    template: '' +
-    '<div>osososo' +
-    '<ul>' +
-    '<li>stuff</li>' +
-    '<li>next</li>' +
-    '</ul>' +
-    '</div>'
+Vue.component('mainNav', {
+    props: ['navTabs'],
+    template:
+    `<!-- Navigation -->
+    <nav class="Navbar">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="Navbar-header">
+            <div class= "Navbar-brand Column--half">
+                <a class="brand" href="#page-top">Sean Teramae</a>
+            </div>
+            <div class="Navbar-tabs Column--half u-text-align-right .u-show-sm">
+                <ul class="tabs">
+                    <li v-for="tab in navTabs">
+                        <a class="page-scroll" v-bind:href="tab.href">{{tab.title}}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>`
 });
 
-new Vue({
+const app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
+        navTabs:[{
+            title: "Skills",
+            href: "#skills"
+        },{
+            title: "Portfolio",
+            href: "#portfolio"
+        },{
+            title: "About",
+            href: "#about"
+        },{
+            title: "Contact",
+            href: "#contact"
+        }]
+    },
+    template :
+        `<div class= "main">
+        <mainNav :navTabs="navTabs" />
+        <div class="Container-black">
+            <div class="CoverPhoto">
+                <div class="CoverPhoto-container">
+                    <div class="CoverPhoto-title u-text-align-center">
+                        <h1>A Personal Portfolio</h1>
+                    </div>
+                    <div class="CoverPhoto-cta">
+                        <button class="CtaMain">Click</button>
+                    </div>                
+                </div>
+            </div>
+        </div>
+    </div>`
 });
+
+
+
