a78019a7e1564694a640e784ce29c43f7e7386b9
Sean Teramae <seteramae@gmail.com>
2020-12-30 16:51:09 -0800

    Updates

---
+++ precache-manifest.d688fb445ddbae3b2ec5c31b8422c2ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3479966372c0d5b0c708cdbb90b8d5af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a9419f15054c320517",
+    "url": "/static/css/main.32fec80d.chunk.css"
+  },
+  {
+    "revision": "77b18770197310a39b33",
+    "url": "/static/js/2.2eddc8ca.chunk.js"
+  },
+  {
+    "revision": "d0a904c481ee8b592e9da2d59a753647",
+    "url": "/static/js/2.2eddc8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a9419f15054c320517",
+    "url": "/static/js/main.4dc25e12.chunk.js"
+  },
+  {
+    "revision": "4c98b2ff2266d13f7f96",
+    "url": "/static/js/runtime-main.5e847b61.js"
+  }
+]);
