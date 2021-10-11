1ce9d0250035d74adcb1475660ccd9a93498ef26
Sean Teramae <seteramae@gmail.com>
2021-10-10 22:15:36 -0700

    Updates

---
+++ precache-manifest.d44347fee1dc0069d8fad435e08eab40.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f62d152ed6fb925f64388bba51929b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e7eac07add8e9ee40ee",
+    "url": "/static/css/main.531fb58a.chunk.css"
+  },
+  {
+    "revision": "8c4b48d9e755b4307536",
+    "url": "/static/js/2.abec5c50.chunk.js"
+  },
+  {
+    "revision": "9769e53c94321964b096711a06084f11",
+    "url": "/static/js/2.abec5c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7eac07add8e9ee40ee",
+    "url": "/static/js/main.ca9e8e19.chunk.js"
+  },
+  {
+    "revision": "27d4f55bff21a72c3f7a",
+    "url": "/static/js/runtime-main.5049960e.js"
+  },
+  {
+    "revision": "770b137d58f2220ffb8914e2913a8588",
+    "url": "/static/media/bond.770b137d.jpg"
+  },
+  {
+    "revision": "91d650883523f3f48b53e4cb9f5eba60",
+    "url": "/static/media/family_christmas.91d65088.jpg"
+  },
+  {
+    "revision": "9f044c40b294ec7cd878e6377eeb5c3d",
+    "url": "/static/media/hearthstone_puzzle.9f044c40.jpg"
+  },
+  {
+    "revision": "ce05f0e7b81bb0b68c1dfe3a1bca0683",
+    "url": "/static/media/hike.ce05f0e7.jpg"
+  },
+  {
+    "revision": "e8e349e20acfecdea4c91de9ca402afa",
+    "url": "/static/media/korea.e8e349e2.jpg"
+  },
+  {
+    "revision": "60bfe1f4618d0d070d3ad5be141e5f9b",
+    "url": "/static/media/mom.60bfe1f4.jpg"
+  },
+  {
+    "revision": "0d80b8597b46a2270d8851be52dd3ac3",
+    "url": "/static/media/questboardsHome.0d80b859.PNG"
+  },
+  {
+    "revision": "956f7d1902c8f8f9aa7f2f38b6aeae44",
+    "url": "/static/media/sophie.956f7d19.jpg"
+  },
+  {
+    "revision": "ff34429004005055da833a5f86a91576",
+    "url": "/static/media/sue_sean.ff344290.jpg"
+  }
+]);
