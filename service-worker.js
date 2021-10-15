dc10d877baa09b6c100b799a8c0a2fbc36f88697
Sean Teramae <seteramae@gmail.com>
2021-10-14 19:26:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d44347fee1dc0069d8fad435e08eab40.js"
+  "/precache-manifest.5c4fae24d710b448105de067a2226ba7.js"
 );
 
 self.addEventListener('message', (event) => {
