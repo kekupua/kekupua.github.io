a7fb04b1222c93213078ebc751955a82b585113c
Sean Teramae <seteramae@gmail.com>
2021-12-29 15:48:22 -0800

    Minor optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import "@kekupua/web-components"; 
-import "@silverlinkz/sl-gallery";
 import './styles/App.css';
 import LandingPage from './pages/LandingPage';
 import HearthstoneRandomCard from './pages/HearthstoneRandomCard';
