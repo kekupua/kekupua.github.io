8845858bcc89f8a7e964ddd332ff11104e125d72
Sean Teramae <seteramae@gmail.com>
2022-02-21 14:16:33 -0800

    feat: Add wild/standard filters to HRC (#3)

--- tools/hearthstone/getCardSets.mjs
+++ tools/hearthstone/getCardSets.mjs
@@ -12,7 +12,9 @@ const createClient = async () => {
 const getCards = async () => {
   const client = await createClient();
   const { data: sets } = await client.metadata({ type: 'sets' });
-  fs.writeFileSync(`./src/lib/hearthstone-metadata.json`, JSON.stringify(sets, null, 2) , 'utf-8');
+  const { data: setGroups } = await client.metadata({ type: 'setGroups' });
+  fs.writeFileSync('./src/lib/hearthstone-set-metadata.json', JSON.stringify(sets, null, 2) , 'utf-8');
+  fs.writeFileSync('./src/lib/hearthstone-set-group-metadata.json', JSON.stringify(setGroups, null, 2) , 'utf-8');
   sets.forEach(async (set) => {
     const {data: cards} = await client.cardSearch({collectible: 1, set: set.slug});
     fs.writeFileSync(`./public/hearthstone/${set.slug}.json`, JSON.stringify(cards, null, 2) , 'utf-8');
