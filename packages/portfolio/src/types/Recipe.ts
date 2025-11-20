2108d19b3838d54e66c71683a5e9550070229539
Sean Teramae <seteramae@gmail.com>
2025-11-19 19:57:44 -0800

    fix: HashRouter and Lassi recipe

--- packages/portfolio/src/types/Recipe.ts
+++ packages/portfolio/src/types/Recipe.ts
@@ -4,7 +4,7 @@ export interface Recipe {
   description: string;
   imageUrl?: string;
   prepTime: string; // e.g., "15 minutes"
-  cookTime: string; // e.g., "30 minutes"
+  cookTime?: string; // e.g., "30 minutes"
   servings: string;
   ingredients: string[];
   instructions: string[];
